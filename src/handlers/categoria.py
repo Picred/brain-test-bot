9fd0cf5eb6018af3f9822020cb793e2efc0444a6
mattiapuglia <101093169+mattiapuglia@users.noreply.github.com>
2023-04-04 23:12:49 +0200

    chore: modifica text

--- src/handlers/categoria.py
+++ src/handlers/categoria.py
@@ -5,8 +5,6 @@ import json
 with open("src/Data/categorie.json", "r") as f:
    categorie=json.load(f)
 
-# Imposta la categoria
 def categoria(update: Update, context: CallbackContext) -> None:
-    # Genera la tastiera per la scelta  delle categorie
     reply_markup = ReplyKeyboardMarkup([ [c] for c in categorie], resize_keyboard=True, one_time_keyboard=True)
-    context.bot.sendMessage(chat_id=update.message.chat_id,text="Scegli il livello di difficoltà:",reply_markup=reply_markup)
+    context.bot.sendMessage(chat_id=update.message.chat_id,text="Scegli la categoria delle domande:",reply_markup=reply_markup)
