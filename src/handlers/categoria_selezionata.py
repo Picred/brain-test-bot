11d3b5b13a24f18ed7dbd033d8a4312b5799cbd8
Picred <andry4you@hotmail.it>
2023-04-13 23:31:02 +0200

    fix: selezione della categoria esclusivamente tramite comando /categoria

--- src/handlers/categoria_selezionata.py
+++ src/handlers/categoria_selezionata.py
@@ -1,20 +1,13 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import CallbackContext
-import json
 
-with open("src/data/categorie.json", "r") as f:
-   categorie=json.load(f)
-
-
-# Salva la categoria selezionato nei dati di User, in context.user_data['categoria']
 def categoria_selezionata(update: Update, context: CallbackContext) -> None:
     categoria = update.message.text
 
-    if(categoria in categorie):
-        # Salva il categoria scelto in input
+    if('categoria' not in context.user_data):
         context.user_data['categoria'] = categoria
-
-        # Messaggio di conferma
-        context.bot.sendMessage(chat_id=update.message.chat_id,text=f"Hai selezionato la categoria {categoria}.")
+        context.bot.sendMessage(chat_id=update.message.chat_id,text=f"Hai selezionato la categoria {categoria}.", reply_markup=ReplyKeyboardRemove())
     else:
-        pass
+        context.bot.sendMessage(chat_id=update.message.chat_id,text=f"La categoria {context.user_data['categoria']} è già stata selezionata.", reply_markup=ReplyKeyboardRemove())
+        context.bot.sendMessage(chat_id=update.message.chat_id,text="Per cambiare la categoria utilizzare /categoria")
+
