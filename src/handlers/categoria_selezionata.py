b444d308acad8734574e40d362172ac4a557f0b6
Picred <andry4you@hotmail.it>
2023-04-13 23:54:00 +0200

    chore: inserito controllo sulla categoria presa in input

--- src/handlers/categoria_selezionata.py
+++ src/handlers/categoria_selezionata.py
@@ -1,8 +1,18 @@
 from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import CallbackContext
+import json
+
+with open("src/data/categorie.json", "r") as f:
+   categorie=json.load(f)
+
 
 def categoria_selezionata(update: Update, context: CallbackContext) -> None:
     categoria = update.message.text
+
+    # Verifico se l'input è coerente con le categorie disponibile
+    if(categoria not in categorie):
+        context.bot.sendMessage(chat_id=update.message.chat_id,text="La categoria inserita non è valida. Utilizzare le categorie disponibili in /categoria", reply_markup=ReplyKeyboardRemove())
+        return
 
     if('categoria' not in context.user_data):
         context.user_data['categoria'] = categoria
@@ -10,4 +20,3 @@ def categoria_selezionata(update: Update, context: CallbackContext) -> None:
     else:
         context.bot.sendMessage(chat_id=update.message.chat_id,text=f"La categoria {context.user_data['categoria']} è già stata selezionata.", reply_markup=ReplyKeyboardRemove())
         context.bot.sendMessage(chat_id=update.message.chat_id,text="Per cambiare la categoria utilizzare /categoria")
-
