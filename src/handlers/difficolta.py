735662ecbd48df10701011266eb9f97ad97dfcb5
Picred <andry4you@hotmail.it>
2023-04-13 23:13:58 +0200

    fix: il livello si modifica solo con /difficolta

--- src/handlers/difficolta.py
+++ src/handlers/difficolta.py
@@ -6,6 +6,9 @@ livelli = ["1: Facile", "2: Intermedio", "3: Difficile"]
 
 # Imposta la difficolta
 def difficolta(update: Update, context: CallbackContext) -> None:
-    # Genera la tastiera per la scelta  dei livelli
+    # Reset della difficoltà
+    if ('livello' in context.user_data):
+        del context.user_data['livello']
+    
     reply_markup = ReplyKeyboardMarkup([ [l] for l in livelli], resize_keyboard=True, one_time_keyboard=True)
     context.bot.sendMessage(chat_id=update.message.chat_id,text="Scegli il livello di difficoltà:",reply_markup=reply_markup)
