be3b4d3ce948fd9b896426242ee30211f9d47d28
Picred <andry4you@hotmail.it>
2023-04-15 12:11:40 +0200

    feat: aggiunte le categorie in relazione alla difficolta' scelta

--- src/handlers/domanda.py
+++ src/handlers/domanda.py
@@ -2,15 +2,26 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 import json
 
-with open('src/data/facile.json', 'r', encoding="utf-8") as f:
-    data = json.load(f)['domande']
-
 def domanda(update: Update, context: CallbackContext) -> None:
+    data = load_file(context)
+
     domanda_corrente = context.user_data['domanda_corrente']
     question = data[domanda_corrente]
     testo = question['testo']
     risposte = question['risposte']
 
-    keyboard = [[InlineKeyboardButton(j['testo'], callback_data=f'{domanda_corrente}:{i}')] for i, j in enumerate(risposte)]
+    keyboard = [[InlineKeyboardButton(j['testo_risposta'], callback_data=f'{domanda_corrente}:{i}')] for i, j in enumerate(risposte)]
     reply_markup = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(chat_id=update.effective_chat.id, text=testo, reply_markup=reply_markup)
+
+def load_file(context: CallbackContext) -> dict:
+    if context.user_data['livello'] == "1: Facile":
+        f = open('src/data/facile.json', 'r', encoding="utf-8")
+    elif context.user_data['livello'] == "2: Intermedio":
+        f = open('src/data/intermedio.json', 'r', encoding="utf-8")
+    elif context.user_data['livello'] == "3: Difficile":
+        f = open('src/data/difficile.json', 'r', encoding="utf-8")
+
+    data = json.load(f)[f"{context.user_data['categoria']}"]
+
+    return data
