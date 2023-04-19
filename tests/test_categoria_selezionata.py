9157c78b830bd46cf9ef4af18d94e7b55af870b4
Picred <andry4you@hotmail.it>
2023-04-19 09:39:22 +0200

    test: aggiunto test di categoria_selezionata

---
+++ tests/test_categoria_selezionata.py
@@ -0,0 +1,21 @@
+from telegram import Update, ReplyKeyboardRemove
+from telegram.ext import CallbackContext
+from pytest_mock import MockerFixture
+
+from src.handlers.categoria_selezionata import categoria_selezionata
+
+
+def test_categoria_selezionata(mocker: MockerFixture):
+    update = mocker.Mock()
+    context = mocker.Mock()
+    context.user_data = {}
+
+    update.message.text = 'Logica'
+    categoria = update.message.text
+    categoria_selezionata(update, context)
+    context.bot.sendMessage.assert_called_with(chat_id=update.message.chat_id,text=f"Hai selezionato la categoria {categoria}.", reply_markup=ReplyKeyboardRemove())
+
+    # categoria già selezionata
+    update.message.text = 'Grammatica'
+    categoria_selezionata(update, context)
+    context.bot.sendMessage.assert_called_with(chat_id=update.message.chat_id,text=f"La categoria {context.user_data['categoria']} è già stata selezionata. \nPer cambiare la categoria utilizzare /categoria", reply_markup=ReplyKeyboardRemove())
