cf45644d396e4e7b08bb6d8379df1e58c3c8d4e4
Picred <andry4you@hotmail.it>
2023-04-21 17:17:52 +0200

    feat: aggiunti commenti finali al quiz

--- src/handlers/risposta.py
+++ src/handlers/risposta.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import CallbackContext
-from src.data.costanti import DOMANDA_CORRENTE, PUNTEGGIO
+from src.data.costanti import DOMANDA_CORRENTE, PUNTEGGIO, CATEGORIA
 from .domanda import domanda, load_file
+import json
 
 def risposta(update: Update, context: CallbackContext) -> None:
     data = load_file(context)
@@ -31,4 +32,22 @@ def risposta(update: Update, context: CallbackContext) -> None:
     else:
         punteggio = context.user_data[PUNTEGGIO]
         context.bot.send_message(chat_id=query.message.chat_id, text=f"Hai terminato il quiz!\nHai totalizzato {punteggio}/{len(data)} punti!")
+        genera_commento(update, context)
 
+
+def genera_commento(update: Update, context: CallbackContext) -> None:
+    
+    with open('src/data/commenti.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)[context.user_data[CATEGORIA]]
+    
+    punteggio = context.user_data[PUNTEGGIO]
+    
+    if punteggio < 2:
+        key = "0"
+    elif 2 <= punteggio < 5:
+        key = "3"
+    elif 3 < punteggio <= 5:
+        key = "5"
+
+    commento = data[0][key]
+    context.bot.sendMessage(chat_id=update.effective_chat.id, text=commento)
