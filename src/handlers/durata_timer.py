00c6412d5a6cacaa12947364b5a713f00977bb22
mattiapuglia <101093169+mattiapuglia@users.noreply.github.com>
2023-04-23 16:06:27 +0200

    feat: implementata funzione durata_timer

---
+++ src/handlers/durata_timer.py
@@ -0,0 +1,11 @@
+from telegram.ext import CallbackContext
+from src.data.costanti import LIVELLO
+
+def durata_timer(context: CallbackContext) -> int:
+    match(context.user_data[LIVELLO]):
+        case '1: Facile':
+            return 10
+        case '2: Intermedio':
+            return 7
+        case '3: Difficile':
+            return 5
