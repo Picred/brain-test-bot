9503ad3f0f0f073dac514a89ca2e7b7a4b3d0750
Picred <andry4you@hotmail.it>
2023-04-24 19:19:19 +0200

    chore: sistemati gli errori di pytest

--- tests/test_categoria_selezionata.py
+++ tests/test_categoria_selezionata.py
@@ -12,9 +12,15 @@ def test_categoria_selezionata(mocker: MockerFixture):
     update.message.text = 'Logica'
     categoria = update.message.text
     categoria_selezionata(update, context)
-    context.bot.sendMessage.assert_called_with(chat_id=update.message.chat_id,text=f"Hai selezionato la categoria {categoria}.", reply_markup=ReplyKeyboardRemove())
+    
+    assert context.bot.sendMessage.call_args[1]['chat_id'] == update.message.chat_id
+    assert context.bot.sendMessage.call_args[1]['text'] == f"Hai selezionato la categoria {categoria}."
+    assert isinstance(context.bot.sendMessage.call_args[1]['reply_markup'], ReplyKeyboardRemove)
 
     # categoria già selezionata
     update.message.text = 'Grammatica'
     categoria_selezionata(update, context)
-    context.bot.sendMessage.assert_called_with(chat_id=update.message.chat_id,text=f"La categoria {context.user_data[CATEGORIA]} è già stata selezionata. \nPer cambiare la categoria utilizzare /categoria", reply_markup=ReplyKeyboardRemove())
+    
+    assert context.bot.sendMessage.call_args[1]['chat_id'] == update.message.chat_id
+    assert context.bot.sendMessage.call_args[1]['text'] == f"La categoria {context.user_data[CATEGORIA]} è già stata selezionata. \nPer cambiare la categoria utilizzare /categoria"
+    assert isinstance(context.bot.sendMessage.call_args[1]['reply_markup'], ReplyKeyboardRemove)
